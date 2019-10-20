fa9aa125fb9b205b6c0ace00b46f29d070aed9ea
Jeri Atkins <jeri.atkins@student.swic.edu>
2019-10-20 05:11:35 -0500

    Solo project, queryselector, value, eventListener

--- scripts.js
+++ scripts.js
@@ -1,6 +1,23 @@
-// Enter a description of this program here.
+// calculates how much item is after the discount is applied.
 
-/**
- * Write some pseudo code steps here.
- * Use those steps as your `commit` messages (and delete them from here as you complete a step.)
- */
+const discount = function(a, b) {
+  return a * (b / 100);
+};
+const price = function(num1, discount) {
+  return num1 - discount;
+};
+
+const form = document.querySelector("form");
+const num1 = document.querySelector("#num-1");
+const num2 = document.querySelector("#num-2");
+const output = document.querySelector("output");
+
+//callback function
+form.addEventListener("submit", function(event) {
+  //Prevent the default browser behavior - DON"T SUBMIT
+  event.preventDefault();
+
+  // form inputs have a property called value
+  console.log(discount);
+  output.textContent = price(num1.value, num2.value);
+});
